d29b989f35257253afe565550023b728ca00cdc8
adonicee <99721625+adonicee@users.noreply.github.com>
2024-09-02 12:38:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message, Sanaz Hassani!');
+    this.reset();
+});
+
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('toggle');
+});
